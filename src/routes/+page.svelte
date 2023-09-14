7bc24eb740f548f8888626c69483d08ad308a5f8
Joey Chan <joeyc.152@gmail.com>
2023-09-14 01:56:48 -0400

    Switch to bun and sveltekit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+</script>
+
+<h1>Joey Chan</h1>
+<p>I am a </p>
+
+<nav>
+    <a href="/resume">Resume</a>
+    <div>Projects</div>
+</nav>
+
+<style>
+</style>
---
+++ src/routes/resume/+page.svelte
@@ -0,0 +1,101 @@
+<h1>Joey Chan</h1>
+
+<h2>Work</h2>
+<div class="employer">TD Bank</div>
+<div class="title">Senior IT Developer</div>
+<div class="date">November 2022 - Present</div>
+<div class="description">
+  <ul>
+    <li>UREFA and CLUA</li>
+    <li>Common code library</li>
+    <li>LDCA</li>
+    <li>microapp</li>
+  </ul>
+</div>
+
+<div class="title">IT Developer III</div>
+<div class="date">November 2022 - November 2022</div>
+
+<div class="title">IT Consultant</div>
+<div class="date">November 2019 - November 2021</div>
+<div class="description">
+  <ul>
+    <li>
+      Developed a mortgage underwriting web application for thousands of users written in Angular
+    </li>
+    <li>
+      Migrated a legacy ETL system to route millions of records per year to separate lines of
+      businesses using the Cinchy platform
+    </li>
+    <li>Lead performance testing of ETL system to provide infrastructure and code was stable</li>
+    <li>
+      Created a Kotlin Spring batch application that directly moved data from files to a Salesforce
+      table
+    </li>
+    <li>
+      Participated in daily scrum and agile practices to focus the team's goals and provide a space
+      to flexibly work around ploblems as they arose
+    </li>
+  </ul>
+</div>
+
+<div class="employer">Environment and Climate Change Canada</div>
+<div class="title">Software Developer</div>
+<div class="date">September 2017 - August 2018</div>
+<div class="description">
+  <ul>
+    <li>
+      Developed a Java component that decoded raw input of weather sensors located in Metro
+      Vancouver into a standard XML format for other products to use, such as the mobile weather app
+      or air traffic control systems
+    </li>
+    <li>
+      Created an Angular website to manipulate generic metadata within systems, targeting scientists
+      and managers as users
+    </li>
+    <li>
+      Collaborated with the team to design an anomaly detection system that automatically notified
+      managers and support staff of production issues, so they could return the system to its
+      original state in a timely fashion
+    </li>
+    <li>
+      Participated with other developers in the validation process of the development life cycle
+    </li>
+    <li>
+      Documented all design and research on a wiki to clearly communicate to other developers the
+      design and purpose of the products that were created
+    </li>
+  </ul>
+</div>
+
+<div class="employer">LBCIT Solutions</div>
+<div class="title">Junior Web Developer</div>
+<div class="date">May 2016 - August 2016</div>
+<div class="description">
+  <ul>
+    <li>
+      Used WordPress, Microsoft .NET, HTML5, CSS3, PHP, C#, JavaScript, and other technologies to
+      create and maintain websites for multiple companies: Six Words Communication, Second Cup,
+      TELUS, WE Charity, Canadian Association for Co- operative Education, and Canadian Board
+      Diversity Council
+    </li>
+    <li>
+      Designed wireframes with Balsmiq Mock-ups, used to propose a website design to a client and
+      gain their business
+    </li>
+    <li>
+      Worked directly with one client to obtain requirements and created a first draft of their
+      website
+    </li>
+  </ul>
+</div>
+
+<h2>Projects</h2>
+
+<h2>School</h2>
+
+<style>
+  .employer {
+    color: red;
+  }
+</style>
